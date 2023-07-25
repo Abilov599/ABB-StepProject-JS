1a8e3c5be0da93747579c066b98a1c939d8f60f1
sadig05 <Sadiq.letifli70@gmail.com>
2023-07-25 18:07:46 +0400

    feat: visit card classes are created

---
+++ src/js/classes/visit/VisitCardiologist.js
@@ -0,0 +1,20 @@
+import Visit from "./Visit.js";
+class VisitCardiologist extends Visit {
+    constructor(fullName, title, description, urgencyLevel, bloodPressure, bodyMassIndex, cardiovascularDiseases, age) {
+      super(fullName, 'Cardiologist', title, description, urgencyLevel);
+      this.bloodPressure = bloodPressure;
+      this.bodyMassIndex = bodyMassIndex;
+      this.cardiovascularDiseases = cardiovascularDiseases;
+      this.age = age;
+    }
+  
+    renderAdditionalInfo() {
+      return `
+        <p class="card-text">Blood Pressure: ${this.bloodPressure}</p>
+        <p class="card-text">Body Mass Index: ${this.bodyMassIndex}</p>
+        <p class="card-text">Cardiovascular Diseases: ${this.cardiovascularDiseases}</p>
+        <p class="card-text">Age: ${this.age}</p>
+      `;
+    }
+  }
+  
